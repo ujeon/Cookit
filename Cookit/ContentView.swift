e317ed71e77df856edd7fb94940ff2010fbc801e
ujeon jang <wj9304@gmail.com>
2023-11-19 14:45:38 +0900

    first commit

---
+++ Cookit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cookit
+//
+//  Created by 우전 on 11/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cookit/CookitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookitApp.swift
+//  Cookit
+//
+//  Created by 우전 on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CookitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
